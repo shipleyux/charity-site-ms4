991f30fefdea9c2e43e27393cc2908c21bfc8622
shipleyux <sarashipley1@gmail.com>
2025-07-07 16:44:06 +0100

    move staff login to footer

--- swa/settings.py
+++ swa/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['swansea-womens-aid-f1ad8ce1317d.herokuapp.com']
+ALLOWED_HOSTS = ['swansea-womens-aid-f1ad8ce1317d.herokuapp.com','127.0.0.1']
 
 
 # Application definition

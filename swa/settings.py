2dd4d9d23e5b87097bc4799437a4206df18973f5
shipleyux <sarashipley1@gmail.com>
2025-07-15 16:16:44 +0100

    add white noise middleware

--- swa/settings.py
+++ swa/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['swansea-womens-aid-f1ad8ce1317d.herokuapp.com','127.0.0.1']
 
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',

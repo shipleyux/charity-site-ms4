7b7fd5c4242bc29991133e5bc905f5cd819868a7
shipleyux <sarashipley1@gmail.com>
2025-07-29 17:44:26 +0100

    Connect to Heroku PostgreSQL

--- swa/settings.py
+++ swa/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import os
+import dj_database_url
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -92,10 +94,11 @@ WSGI_APPLICATION = 'swa.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgres://localhost/charity_site_ms4_db',
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
